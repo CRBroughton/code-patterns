6b5081731713ea99c6030f39907199eb5b991367
CRBroughton <crbroughton@posteo.uk>
2022-09-13 17:54:30 +0100

    docs(typescript): :memo: simplified typescript api client

--- typescript/api/api-client-axios/index.ts
+++ typescript/api/api-client-axios/index.ts
@@ -3,7 +3,7 @@ import { ApiClient, handleResponse } from './api'
 const client = new ApiClient()
 
 const results = async() => {
-    const test = await client.getResults()
+    const test = await client.initResults()
     if (test) {
         // This destructuring is dependent on the response handler
         const { response } = handleResponse(test)
